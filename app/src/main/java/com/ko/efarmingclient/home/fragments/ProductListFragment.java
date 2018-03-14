ff6281b7e8a9005f374eab7bd924b792075cd954
kovartthan <kovartthan@github.com>
2018-03-14 19:57:54 +0530

    second

--- app/src/main/java/com/ko/efarmingclient/home/fragments/ProductListFragment.java
+++ app/src/main/java/com/ko/efarmingclient/home/fragments/ProductListFragment.java
@@ -3,13 +3,19 @@ package com.ko.efarmingclient.home.fragments;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -29,6 +35,8 @@ public class ProductListFragment extends Fragment implements OnChatOpenListener 
     private RecyclerView recyclerView;
     private ArrayList<ProductInfo> productInfoArrayList;
     private ProductListAdapter productListAdapter;
+    private FloatingActionButton fabFilter;
+    private String filterLocation;
 
     public ProductListFragment() {
 
@@ -51,17 +59,18 @@ public class ProductListFragment extends Fragment implements OnChatOpenListener 
         productInfoArrayList = new ArrayList<>();
         productListAdapter = new ProductListAdapter(getActivity(), productInfoArrayList);
         productListAdapter.setOnChatOpenListener(this);
+        fabFilter = view.findViewById(R.id.fab_filter);
         recyclerView.setAdapter(productListAdapter);
     }
 
     private void setupDefault() {
         getProductListFromPublicDataBase();
-
     }
 
+    ValueEventListener firstValueListener;
+
     private void getProductListFromPublicDataBase() {
-        FirebaseDatabase.getInstance()
-                .getReference().child(Constants.PRODUCT_INFO).addValueEventListener(new ValueEventListener() {
+        firstValueListener = new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 productInfoArrayList = new ArrayList<>();
@@ -72,13 +81,22 @@ public class ProductListFragment extends Fragment implements OnChatOpenListener 
                     }
                 }
                 productListAdapter.updateList(productInfoArrayList);
+                if (productInfoArrayList.size() > 0) {
+                    fabFilter.setVisibility(View.VISIBLE);
+                } else {
+                    fabFilter.setVisibility(View.GONE);
+                }
             }
 
             @Override
             public void onCancelled(DatabaseError databaseError) {
                 Log.e("TAG", "onCancelled" + databaseError);
             }
-        });
+        };
+
+        FirebaseDatabase.getInstance()
+                .getReference().child(Constants.PRODUCT_INFO).addValueEventListener(firstValueListener);
+
         for (ProductInfo productInfo : productInfoArrayList) {
             Log.e("TAG", "" + productInfo.productName);
         }
@@ -86,11 +104,120 @@ public class ProductListFragment extends Fragment implements OnChatOpenListener 
     }
 
     private void setupEvent() {
+        fabFilter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showFilterAlert();
+            }
+        });
+    }
 
+    private Spinner dynamicSpinner;
+    private TextView txtSubmit;
+    private TextView txtCancel;
+
+    private void showFilterAlert() {
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = this.getLayoutInflater();
+        final View dialogView = inflater.inflate(R.layout.alert_filter, null);
+        dialogBuilder.setView(dialogView);
+        final AlertDialog alertDialog = dialogBuilder.create();
+        alertDialog.setCancelable(false);
+        dynamicSpinner = dialogView.findViewById(R.id.dynamic_spinner);
+        txtSubmit = dialogView.findViewById(R.id.txt_submit);
+        txtCancel = dialogView.findViewById(R.id.txt_cancel);
+
+        ArrayList<String> locationFilterList = getLocationFilter();
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(),
+                android.R.layout.simple_spinner_item, locationFilterList);
+
+        dynamicSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                filterLocation = (String) parent.getItemAtPosition(position);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+        dynamicSpinner.setAdapter(adapter);
+
+        txtSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alertDialog.dismiss();
+                getFilteredProductFromDb();
+            }
+        });
+
+        txtCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alertDialog.dismiss();
+            }
+        });
+
+        alertDialog.show();
+
+    }
+
+    private ArrayList<String> getLocationFilter() {
+        final ArrayList<String> locationFilterList = new ArrayList<>();
+        FirebaseDatabase.getInstance().getReference().child("location_filter").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    String location = snapshot.getValue().toString();
+                    locationFilterList.add(location);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+        return locationFilterList;
+    }
+
+    ValueEventListener valueEventListener;
+
+    private void getFilteredProductFromDb() {
+        FirebaseDatabase.getInstance().getReference().removeEventListener(firstValueListener);
+
+        valueEventListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                productInfoArrayList = new ArrayList<>();
+                if (dataSnapshot != null) {
+                    for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                        ProductInfo productInfo = snapshot.getValue(ProductInfo.class);
+                        if (productInfo.company_info.location.equalsIgnoreCase(filterLocation))
+                            productInfoArrayList.add(productInfo);
+                    }
+                }
+                productListAdapter.updateList(productInfoArrayList);
+                if (productInfoArrayList.size() > 0) {
+                    fabFilter.setVisibility(View.VISIBLE);
+                } else {
+                    fabFilter.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.e("TAG", "onCancelled" + databaseError);
+            }
+        };
+        FirebaseDatabase.getInstance()
+                .getReference().child(Constants.PRODUCT_INFO).addValueEventListener(valueEventListener);
     }
 
     @Override
     public void openChat(ProductInfo productInfo) {
-        startActivity(new Intent(getActivity(), ChatActivity.class).putExtra("Product_id",productInfo));
+        startActivity(new Intent(getActivity(), ChatActivity.class).putExtra("Product_id", productInfo));
     }
 }
